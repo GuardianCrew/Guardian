42b3786bc1ccb01855ddf395d9b5a2bc4659beb4
hapily04 <87914807+hapily04@users.noreply.github.com>
2023-01-22 21:47:57 -0600

    Generic changes as requested by UnderscoreTud

--- src/main/java/com/github/guardiancrew/util/Reflect.java
+++ src/main/java/com/github/guardiancrew/util/Reflect.java
@@ -10,27 +10,28 @@ import java.util.List;
 
 public class Reflect {
 
+    @SuppressWarnings("unchecked")
     public static <T> Class<? extends T>[] getClasses(String pkg, Class<T> clazz) throws ClassNotFoundException, URISyntaxException, IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        List<Class<?>> classes = new ArrayList<>();
+        List<Class<? extends T>> classes = new ArrayList<>();
         String fileFormattedPkg = pkg.replace('.', '\\');
         for (Iterator<URL> it = classLoader.getResources(fileFormattedPkg).asIterator(); it.hasNext();) {
             URL url = it.next();
             File pkgAsFile = new File(url.toURI());
             File[] files = pkgAsFile.listFiles();
-            if (files == null) return null;
+            if (files == null) return (Class<? extends T>[]) new Class<?>[0];
             for (File file : files) {
                 String fString = file.toString();
                 if (fString.endsWith(".class") && !(fString.endsWith(clazz.getSimpleName()))) {
                     String className = fString.substring(fString.lastIndexOf('\\')+1);
                     Class<?> claz = Class.forName(pkg + "." + className.replaceAll(".class", ""));
                     if (clazz.isAssignableFrom(claz)) {
-                        classes.add(claz);
+                        classes.add((Class<? extends T>) claz);
                     }
                 }
             }
         }
-        return classes.toArray(new Class<?>[0]);
+        return (Class<? extends T>[]) classes.toArray(new Class<?>[0]);
     }
 
 }
