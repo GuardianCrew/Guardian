14745936c37c1967cad76ddca57be9e1f883daf2
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2023-01-23 22:12:31 -0500

    Punishment + Staff Mode Stuff (#2)

--- src/main/java/com/github/guardiancrew/wrapper/GuardianPlayer.java
+++ src/main/java/com/github/guardiancrew/wrapper/GuardianPlayer.java
@@ -1,18 +1,25 @@
 package com.github.guardiancrew.wrapper;
 
 import com.github.guardiancrew.punishment.Punishment;
+import com.github.guardiancrew.punishment.PunishmentType;
 import com.github.guardiancrew.staffmode.StaffModeTools;
 import com.github.guardiancrew.util.TextUtils;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class GuardianPlayer {
 
     private final Player bukkitPlayer;
     private boolean muted = false;
+    private boolean staffMode = false;
+    private List<Punishment> punishments = new ArrayList<>();
 
     GuardianPlayer(Player bukkitPlayer) {
         this.bukkitPlayer = bukkitPlayer;
@@ -34,23 +41,27 @@ public class GuardianPlayer {
         switch (punishment.getType()) {
             case KICK:
                 getBukkitPlayer().kickPlayer("Kick Message: The message will be in the config.");
+                getPunishments().add(punishment);
                 break;
             case MUTE:
                 muted = true;
                 getBukkitPlayer().sendMessage("Mute Message: The message will be in the config.");
+                getPunishments().add(punishment);
                 break;
             case BAN:
                 if (punishment.isIp())
                     Bukkit.banIP(bukkitPlayer.getAddress().toString());
                 else
                     Bukkit.getBanList(BanList.Type.NAME).addBan(getBukkitPlayer().getName(), "Ban Message: The message will be in the config.", Date.from(punishment.getLength()), "Source Thing");
+                getPunishments().add(punishment);
                 break;
         }
     }
 
     public void staffMode(boolean activated) {
         if (activated) {
-            getBukkitPlayer().saveData();
+            if (!staffMode)
+                getBukkitPlayer().saveData();
             getBukkitPlayer().getInventory().clear();
             getBukkitPlayer().sendMessage(TextUtils.translate("&3&lGuardian&3: &cThis message (maybe the prefix too?) can be edited in the config."));
             for (StaffModeTools tool : StaffModeTools.values()) {
@@ -59,5 +70,15 @@ public class GuardianPlayer {
         } else {
             getBukkitPlayer().loadData();
         }
+        staffMode = activated;
     }
+
+    public boolean inStaffMode() {
+        return staffMode;
+    }
+
+    public List<Punishment> getPunishments() {
+        return punishments;
+    }
+
 }
